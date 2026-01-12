2d64d39c984ecb6054ebb8305dfc596345f19003
Sebitas <sebasssalazar98765@gmail .com>
2026-01-11 23:57:12 -0400

    Fix Casual 23

--- web-app/client/vite.config.js
+++ web-app/client/vite.config.js
@@ -2,21 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@bootstrap': path.resolve(__dirname, 'src/assets/bootstrap')
+      // Mantenemos tus alias por si los usas en el código
+      '@': path.resolve(__dirname, './src'),
+    }
+  },
+  build: {
+    // ESTO ES LO QUE SOLUCIONA EL ERROR EN VERCEL
+    rollupOptions: {
+      external: [
+        '/bootstrap/js/bootstrap.bundle.min.js',
+        '/bootstrap/css/bootstrap.min.css'
+      ]
     }
   },
   server: {
     port: 5173,
-    strictPort: true, // No intentar otros puertos si 5173 está ocupado
-    host: true, // Exponer a la red
-    open: false, // No abrir automáticamente el navegador
     proxy: {
-      // Proxy para las solicitudes a la API
       '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
